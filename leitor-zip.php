6c40a40788f78cef2784c7299cb4f0b3c5040c0e
Eduardo Teixeira <dunirvana@gmail.com>
2022-12-15 08:32:24 -0300

    Contexto em streams

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,3 +1,16 @@
 <?php
 
-echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+// para abrir um zip sem senha
+//echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123456'
+    ]
+]);
+
+echo file_get_contents(
+    'zip://arquivos.zip#lista-cursos.txt',
+    false,
+    $contexto
+);
